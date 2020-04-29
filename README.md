5b8223e7ecfadd2644c43b9904cba93708e14d23
freddy-crawford-dev <freddy.crawford@ruralsourcing.com>
2020-04-29 15:56:11 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# coinMarketApp" 
+"# coinMarketApp" 
---
+++ coincapglobal.py
@@ -0,0 +1,24 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/global-metrics/quotes/latest'
+# parameters = {
+#   'start':'1',
+#   'limit':'50',
+#   'convert':'USD'
+# }
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'cd082a5f-c077-496e-8113-6180e67d44a4',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url)
+  data = response.json()
+  print(json.dumps(data, sort_keys=True, indent=4))
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
